3b1c8b47039ef8b3a6a3a4d8061dd4b34c814a85
Danny Burrows <burrows.danny@gmail.com>
2014-05-18 19:38:03 -0500

    finished program

--- Spring2014/CS372/Week7/ftclient.py
+++ Spring2014/CS372/Week7/ftclient.py
@@ -1,11 +1,159 @@
-import socket
+#!/usr/bin/python
+# Danny Burrows
+# CS372 - Homework 1
+# ftserve.py
 
-HOST = 'localhost'
-PORT = 30021
+from socket import *
+import sys
+import os
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
-s.sendall("This is a test")
-data = s.recv(1024)
-s.close()
-print "Received\n", repr(data)
+# Two connections: 1 for control, 1 for file transfer
+# Control - Port 30021, Data - Port 30020
+
+MAXMSG = 1024
+
+class client(object):
+	"""
+	Client class, which accepts input from the user and interfaces with the server via two connections, the data and control sockets.
+	"""
+	def __init__(self, host="localhost", controlPort=30021, dataPort=30020, cli=True):
+		# control socket info
+		self.controlSock = socket(AF_INET, SOCK_STREAM) # create socket
+		try:
+			self.controlSock.connect((host, controlPort)) # connect to socket
+		except:
+			self.errorMsg("The server is not available")
+		# data socket info
+		self.dataSock = socket(AF_INET, SOCK_STREAM)
+		self.dataSock.bind(('', dataPort))
+		self.dataSock.listen(1)
+		# inform server client is ready
+		self.controlSock.send("rdy")
+		self.data, self.dataAddr = self.dataSock.accept()
+		# inform the IP/port connected
+		print "Connected to ", self.dataAddr
+		# run the main loop
+		if not cli:
+			self.mainLoop()
+
+	def mainLoop(self):
+		"""
+		Gets commands from the user, loops until the user quits
+		"""
+		# a list of commands that the server can handle
+		properCommands = ['list', 'quit', 'get', 'cd']
+		while True:
+			command = raw_input("cmd > ")
+			# ensures that command recieved is one that the server can handle
+			if command.split()[0] not in properCommands or (command.split()[0] == "get" and len(command.split()) < 2):
+				self.printUsage()
+			else:
+				# command is good, send command to the server
+				self.controlSock.send(command)
+				# receive response
+				response = self.controlSock.recv(MAXMSG)
+				if response == "quit":
+					self.quit()
+				elif response[0:3] == "put":
+					self.getFile(response.split()[1], response.split()[2])
+				else:
+					print response
+	
+	def processCommand(self, command, file):
+		"""
+		Handler for processing a command, dictates what action occurs with the client object
+		"""
+		# a file request
+		if command == "get":
+			msg = command + " " + file
+			self.controlSock.send(msg)
+			response = self.controlSock.recv(MAXMSG)
+			self.getFile(response.split()[1], response.split()[2])
+		# list contents of the directory
+		elif command == "list":
+			self.controlSock.send(command)
+			response = self.controlSock.recv(MAXMSG)
+		# displays the servers respnse
+		print response
+		self.controlSock.send("quit")
+		self.quit()
+
+	def getFile(self, fileName, fileSize):
+		"""
+		Handler for receiving a file, calls the get() method which actually does the lifting
+		"""
+		# receives the
+		self.get(fileName, fileSize)
+		# let's server know we are finished writing
+		self.controlSock.send("end")
+
+	def quit(self):
+		"""
+		Shuts down the client and sends a command to the server to shut down.
+		"""
+		self.controlSock.close()
+		self.dataSock.close()
+		sys.exit()
+
+	def printUsage(self):
+		"""
+		Displays how to use the command interface. Displays on error.
+		"""
+		print "Commands supported:"
+		print "list - provides a listing of the files and directories on the server path"
+		print "cd - changes the directory on the server"
+		print "get <filename> - pulls a text file from the server and stores it in the local directory"
+		print "quit - shuts down the server and the client"
+		return
+
+	def errorMsg(self, message):
+		"""
+		General error message.
+		"""
+		print message
+		print "Check your arguments and try again"
+		sys.exit()
+
+	def get(self, fileName, fileSize):
+		"""
+		Receives the file sent from the server.
+		"""
+		response = ""
+		message = ""
+		# read the file contents in
+		while len(message) < int(fileSize):
+			bytesLeft = int(fileSize) - len(message)
+			response = self.data.recv(bytesLeft)
+			message = message + response
+		# writes the file
+		print "Writing new file..."
+		if os.path.isfile(fileName):
+			print "File already exists."
+		else:
+			output = open(fileName, 'w')
+			output.write(message)
+			print "File " + fileName + " successfully transferred."
+
+		return
+
+def main(args):
+	# simple parsing of te command line arguments.
+	try:		
+		if len(args) > 1:
+			ftclient = client(host=args[1], controlPort=int(args[2]))
+			if len(args) == 5:
+				file = args[4]
+			else:
+				file = None
+			ftclient.processCommand(args[3], file)
+		else:
+			ftclient = client(cli=False)
+	except:
+		# an error occurred
+		print "Please check your parameters."
+		print "Proper usage is: python ftclient <ip address> <port> <command> <?file>"
+		exit()
+
+# main program
+if __name__ == "__main__":
+	main(sys.argv)
--- Spring2014/CS372/Week7/ftserve.py
+++ Spring2014/CS372/Week7/ftserve.py
@@ -1,20 +1,128 @@
+#!/usr/bin/python
 # Danny Burrows
+# CS372 - Homework 1
 # ftserve.py
 
-import socket
+from socket import *
+import os
+import sys
 
-HOST = ''
-CONTROLPORT = 30021
-DATAPORT = 30020
+MAXMSG = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, CONTROLPORT))
-s.listen(1)
-while 1:
-	connection, address = s.accept()
-	print "Connected to", address
-	data = connection.recv(1024)
-	if not data: break
-	connection.sendall(data)
+class controlSock:
+	"""
+	Class for the controlling socket
+	"""
+	def __init__(self, controlPort=30021, dataPort=30020):
+		"""
+		Initalize the class
+		"""
+		self.dataPort = dataPort
+		self.connect = None
+		self.controlSock = socket(AF_INET, SOCK_STREAM)
+		self.controlPort = controlPort
+		print "Server is listening"
+		self.controlSock.bind(("", self.controlPort))
+		self.controlSock.listen(1)
+		self.mainLoop()
 
-connection.close()
+	def mainLoop(self):
+		"""
+		Main handler for the controlSock class. Loops until a quit message is recieved from the client.
+		Sends commands and parses responses from the server.
+		"""
+		while True:
+			# connection is the object that will control sending/receiving
+			# address is a tuple of the socket, IP first, port second
+			self.connection, address = self.controlSock.accept()
+			print "Server Status: Ready"
+			print "Connected to: ", address
+			request = self.connection.recv(MAXMSG)
+			# client is ready
+			if request == "rdy":
+				self.dataSock = dataSock(address[0],self.dataPort)
+			while True:
+				# error message will print on the client if an error is found anywhere
+				errorMsg = None
+				# msg will print on the client if everything goes smoothly
+				msg = ""
+
+				# received the command and call functions
+				command = self.connection.recv(MAXMSG)
+				print "Command > ", command
+				# list the current directory on the server
+				if command == "list":
+					msg = self.listDir()
+				# change the directory on the server
+				elif command[0:2] == "cd":
+					# get just the directory name
+					newDir = command[3:]
+					# try to change the directory, throws an error if directory does not exist
+					try:
+						os.chdir(newDir)
+						msg = self.listDir()
+					# catch error
+					except:
+						errorMsg = "Directory does not exist"
+				# shut down the server and the client
+				elif command == "quit":
+					self.shutDown()
+				elif command[0:3] == "get":
+					fileName = command.split()[1]
+					if os.path.isfile(fileName):
+						fileSize = os.path.getsize(fileName)
+						ready = "put " + fileName + " " + str(fileSize)
+						self.connection.send(ready)
+						self.dataSock.put(fileName, fileSize)
+						if self.connection.recv(MAXMSG) == "end":
+							print "File uploaded"
+					else:
+						errorMsg = "File does not exist"
+
+				# return the proper message to the client
+				if errorMsg:
+					self.connection.send(errorMsg)
+				else:
+					self.connection.send(msg)
+
+	def listDir(self):
+		"""
+		Lists the current directory on the server.
+		"""
+		directory = os.listdir(os.curdir)
+		ls = "" # empty string for directory
+		for file in directory:
+			ls = ls + file + "\n"
+		return ls
+
+	def shutDown(self):
+		"""
+		Closes the control socket and quits the program.
+		"""
+		# send a message to shut the client down too
+		self.connection.send("quit")
+		# shut the server down
+		self.controlSock.close()
+		sys.exit()
+
+class dataSock:
+	"""
+	The socket for the datastream on the server. 
+	"""
+	def __init__(self, host, dataPort):
+		self.dataSock = socket(AF_INET, SOCK_STREAM)
+		self.dataSock.connect((host, dataPort))
+
+	def put(self, fileName, fileSize):
+		"""
+		Reads a file byte by byte, sends the data over the socket connection
+		"""
+		# read file in
+		input = open(fileName, 'r').read()
+		sent = 0 # number of bytes sent
+		while sent < fileSize:
+			read = self.dataSock.send(input[sent:])
+			sent = sent + read
+
+if __name__ == "__main__":
+	controlSock()
